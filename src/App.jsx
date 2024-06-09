7b8f64314744c3e1d8e8ba071f8baba29634f492
DutchakKristina <dutchakk7@gmail.com>
2024-06-08 23:58:45 -0600

    Added new folders and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
-import Layout from '../components/Layout/Layout';
+import Layout from './components/Layout/Layout';
 import { lazy, Suspense } from 'react';
 
-const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
-const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
-const MovieDetailsPage = lazy(() => import('../pages/MovieDetailsPage/MovieDetailsPage'));
-const MovieReviews = lazy(() => import('../components/MovieReviews/MovieReviews'));
-const MovieCast = lazy(() => import('../components/MovieCast/MovieCast'));
-const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage/MovieDetailsPage'));
+const MovieReviews = lazy(() => import('./components/MovieReviews/MovieReviews'));
+const MovieCast = lazy(() => import('./components/MovieCast/MovieCast'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
 function App() {
   return (
